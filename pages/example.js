9653be6be0948ccef2cf0d59809d28626e6e4baa
Mark Jackson <markjackson02@ymail.com>
2020-09-06 20:19:39 -0400

    wip [skip ci]

--- pages/example.js
+++ pages/example.js
@@ -1,17 +1,65 @@
+import useSWR from 'swr'
 import Link from 'next/link'
+import { useUser } from '../utils/auth/useUser'
+import Logo from '../components/logo/Logo';
 
-const Example = (props) => {
+const fetcher = (url, token) =>
+  fetch(url, {
+    method: 'GET',
+    headers: new Headers({ 'Content-Type': 'application/json', token }),
+    credentials: 'same-origin',
+  }).then((res) => res.json())
+
+const Index = () => {
+  const { user, logout } = useUser()
+  const { data, error } = useSWR(
+    user ? ['/api/getFood', user.token] : null,
+    fetcher
+  )
+  if (!user) {
+    return (
+      <>
+        <p>Hi there!</p>
+        <p>
+          You are not signed in.{' '}
+          <Link href={'/auth'}>
+            <a>Sign in</a>
+          </Link>
+        </p>
+      </>
+    )
+  }
+
   return (
     <div>
-      <p>
-        This page is static because it does not fetch any data or include the
-        authed user info.
-      </p>
-      <Link href={'/'}>
-        <a>Home</a>
-      </Link>
+      <Logo />
+      <div>
+        <p>You're signed in. Email: {user.email}</p>
+        <p
+          style={{
+            display: 'inline-block',
+            color: 'blue',
+            textDecoration: 'underline',
+            cursor: 'pointer',
+          }}
+          onClick={() => logout()}
+        >
+          Log out
+        </p>
+      </div>
+      <div>
+        <Link href={'/example'}>
+          <a>Another example page</a>
+        </Link>
+      </div>
+      {error && <div>Failed to fetch food!</div>}
+      {data && !error ? (
+        <div>Your favorite food is {data.food}.</div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   )
 }
 
-export default Example
+export default Index
---
+++ pages/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Box, Flex } from '@chakra-ui/core'
+import {}
+
+const index = (props) => {
+    const { } = props;
+    return (
+        <Box
+            maxW="960px"
+        >
+            <Flex id="navbar">
+                <Logo />
+
+            </Flex>
+        </Box>
+    );
+};
+
+index.propTypes = {
+
+};
+
+index.defaultProps = {
+
+};
+
+export default index;
