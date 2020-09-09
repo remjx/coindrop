6ee6e411df810c086e5f7d7f7aac57849a5d4a2e
Mark Jackson <markjackson02@ymail.com>
2020-09-09 10:41:22 -0400

    update landing page / responsiveness

--- components/logo/Logo.jsx
+++ components/logo/Logo.jsx
@@ -3,20 +3,12 @@ import { Flex, Image, Text, useTheme } from '@chakra-ui/core';
 
 const Logo = () => {
     const theme = useTheme();
-<<<<<<< HEAD
     const logoSize = ["48px", "64px"]
     return (
         <Flex ml={2} align="center">
             <Image src="/piggy-question-256.png" width={logoSize} height={logoSize} />
             <Text
                 fontSize={["4xl", "5xl"]}
-=======
-    return (
-        <Flex ml={2} align="center">
-            <Image src="/piggy-question-256.png" width="64px" height="64px" />
-            <Text
-                fontSize="5xl"
->>>>>>> eeffc353986c4233aa22a7055b78e7c5a6d9989e
                 fontFamily="Changa"
                 fontWeight={500}
                 color={theme.colors.gray['500']}
