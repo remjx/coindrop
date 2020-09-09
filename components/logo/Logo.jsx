d1c82e936666b8bd9f21e4022dd64c9374a2e6c9
Mark Jackson <markjackson02@ymail.com>
2020-09-09 10:39:22 -0400

    wip [skip ci]

--- components/logo/Logo.jsx
+++ components/logo/Logo.jsx
@@ -3,11 +3,12 @@ import { Flex, Image, Text, useTheme } from '@chakra-ui/core';
 
 const Logo = () => {
     const theme = useTheme();
+    const logoSize = ["48px", "64px"]
     return (
         <Flex ml={2} align="center">
-            <Image src="/piggy-question-256.png" width="64px" height="64px" />
+            <Image src="/piggy-question-256.png" width={logoSize} height={logoSize} />
             <Text
-                fontSize="5xl"
+                fontSize={["4xl", "5xl"]}
                 fontFamily="Changa"
                 fontWeight={500}
                 color={theme.colors.gray['500']}
