137ef5b40ab2d169fb7ad6fdd17af0871947f5ea
Mark Jackson <markjackson02@ymail.com>
2020-09-17 21:06:09 -0400

    wip [skip ci]

--- components/Dashboard/UserOwnedPiggybanks.jsx
+++ components/Dashboard/UserOwnedPiggybanks.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import useSWR from 'swr';
-import { Text, Box, Heading, Spinner } from '@chakra-ui/core';
+import { Box, Heading, Spinner, Stack } from '@chakra-ui/core';
 import { db } from '../../utils/client/db';
 
 async function fetchUserOwnedPiggybanks(uid) {
@@ -20,23 +20,52 @@ async function fetchUserOwnedPiggybanks(uid) {
     return piggybankData;
 }
 
+function Piggybank({ name }) {
+    return (
+        <Box p={5} shadow="md" borderWidth="1px" mt={3}>
+            <Heading fontSize="xl">{name}</Heading>
+            {/* <Text mt={4}>{desc}</Text> */}
+        </Box>
+    );
+}
+Piggybank.propTypes = {
+    name: PropTypes.string.isRequired,
+};
+
+function PiggybankContainer({ children }) {
+    return (
+        <Stack spacing={8} my={4}>
+            {children}
+        </Stack>
+    );
+}
+PiggybankContainer.propTypes = {
+    children: PropTypes.element.isRequired,
+};
+
 const UserOwnedPiggybanks = (props) => {
     const { uid } = props;
     const { data, error } = useSWR(uid, fetchUserOwnedPiggybanks);
+    const PiggybankContent = () => {
+        if (error) {
+            console.error(error);
+            return `Error getting piggybank data, please try refreshing the page.`;
+        }
+        if (data) {
+            return (
+                <PiggybankContainer>
+                    {data.map(piggybank => <Piggybank name={piggybank.piggybankName} />)}
+                </PiggybankContainer>
+            );
+        }
+        return <Spinner size="lg" />;
+    };
     return (
         <Box>
             <Heading>
-                User owned piggybanks:
+                My Piggybanks
             </Heading>
-            {error ? `Error getting piggybank data, please try again ${error}` : 
-                data ?
-                    data.map(piggybank => (
-                        <Heading as="h5" size="sm">
-                            {piggybank.piggybankName}
-                        </Heading>
-                    ))
-                : <Spinner size="lg" />
-            }
+            <PiggybankContent />
         </Box>
     );
 };
