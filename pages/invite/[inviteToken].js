b443cb57421833c2185bf9b10c3d4508f53fc9dc
Mark Jackson <markjackson02@ymail.com>
2020-09-28 02:45:05 -0400

    wip [skip ci]

---
+++ pages/invite/[inviteToken].js
@@ -0,0 +1,5 @@
+// render landing page
+// store invite token in context
+// on landing page, show a welcome bar that invite is pending login
+// once user is logged in, on dashboard, if token exists in context, trade the token for a collections('invites').email
+// confirm user that invite has been processed successfully.
