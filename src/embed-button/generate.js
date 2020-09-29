0efa810e39e27a82d52a7f63d7920e5f8f866907
Mark Jackson <markjackson02@ymail.com>
2020-09-28 22:09:23 -0400

    wip [skip ci]

---
+++ src/embed-button/generate.js
@@ -0,0 +1,16 @@
+const minify = require('minify');
+const fs = require('fs');
+const path = require('path');
+
+async function combineAndGenerateScript() {
+    const cssMinified = await minify(path.join(__dirname, 'embed-button.css'));
+    const jsMinified = await minify(path.join(__dirname, 'embed-button.js'));
+    const styleJs = `
+        const styles = \`${`<style>${cssMinified}</style>`}\`;
+        document.write(styles);
+    `;
+    const data = styleJs + jsMinified;
+    fs.writeFileSync('./public/embed-button.js', data);
+}
+
+combineAndGenerateScript();
