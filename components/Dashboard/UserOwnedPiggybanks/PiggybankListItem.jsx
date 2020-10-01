5bfa8544ee924e11d4891f4ce13919bf67f1c59f
Mark Jackson <markjackson02@ymail.com>
2020-10-01 12:02:19 -0400

    removed unneeded PiggybankListItem buttons but still a stack. going to try to convert to cards

--- components/Dashboard/UserOwnedPiggybanks/PiggybankListItem.jsx
+++ components/Dashboard/UserOwnedPiggybanks/PiggybankListItem.jsx
@@ -1,61 +1,51 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import NextLink from 'next/link';
-import { Link, Box, Flex, Heading, Button, useTheme } from '@chakra-ui/core';
-import CopyLinkShareButton from '../../Buttons/CopyLinkShareButton';
+import { Spinner, Box, Link, PseudoBox, Flex, Heading, useTheme } from '@chakra-ui/core';
 
 function PiggybankListItem({ id }) {
     const { colors } = useTheme();
-    const publicUrl = `coindrop.to/${id}`;
     const [isLoading, setIsLoading] = useState();
     return (
-        <Flex
-            p={5}
-            shadow="md"
-            borderWidth="1px"
-            borderRadius="10px"
+        <PseudoBox
+            onClick={() => setIsLoading(true)}
+            cursor="pointer"
             mt={3}
-            justify="space-between"
-            wrap="wrap"
-            align="center"
+            bg={isLoading ? colors.gray['200'] : undefined}
+            _hover={{
+                bg: colors.gray['100'],
+                textDecoration: "none",
+            }}
+            _active={{
+                bg: colors.gray['200'],
+            }}
         >
-            <Flex
-                align="center"
-            >
-                <NextLink href={`/${id}`} passHref>
-                    <Link style={{textDecorationColor: colors.orange['500']}}>
+            <NextLink href={`/${id}`} passHref>
+                <a>
+                    <Box
+                        py={5}
+                        shadow="md"
+                        borderWidth="1px"
+                        borderRadius="10px"
+                        textAlign="center"
+                    >
                         <Heading fontSize="xl">
-                            coindrop.to/
-                            {id}
+                            {isLoading
+                            ? (
+                                <Flex align="center" justify="center">
+                                    <Spinner />
+                                    <Box ml={2}>
+                                        Loading
+                                    </Box>
+                                </Flex>
+                            ) : (
+                                `coindrop.to/${id}`
+                                )}
                         </Heading>
-                    </Link>
-                </NextLink>
-            </Flex>
-            <Flex wrap="wrap">
-                <Box
-                    m={1}
-                >
-                    <CopyLinkShareButton textToCopy={publicUrl} />
-                </Box>
-                <Box
-                    m={1}
-                    onClick={() => setIsLoading(true)}
-                >
-                    <NextLink href={`/${id}`} passHref>
-                        <Link style={{textDecoration: 'none'}}>
-                            <Button
-                                leftIcon="view"
-                                role="link"
-                                isLoading={isLoading}
-                                loadingText="Loading"
-                            >
-                                View
-                            </Button>
-                        </Link>
-                    </NextLink>
-                </Box>
-            </Flex>
-        </Flex>
+                    </Box>
+                </a>
+            </NextLink>
+        </PseudoBox>
     );
 }
 PiggybankListItem.propTypes = {
