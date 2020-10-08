663f0fb33ee717d37f3e836e4db5edb2d7848638
Mark Jackson <markjackson02@ymail.com>
2020-10-08 09:20:26 -0400

    wip [skip ci]

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -12,6 +12,14 @@ class MyDocument extends Document {
         <Head>
             <link href="https://fonts.googleapis.com/css2?family=Changa:wght@500&display=swap" rel="stylesheet" />
             <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400&display=swap" rel="stylesheet" /> {/* used for embed button */}
+            {/* generated with https://realfavicongenerator.net/ */}
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+            <link rel="manifest" href="/favicon/site.webmanifest" />
+            <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+            <meta name="msapplication-TileColor" content="#ffffff" />
+            <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
