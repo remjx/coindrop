8cdb3e21a13aceac8bd3b6e734b7f41e5a153db4
Mark Jackson <35121685+markjackson02@users.noreply.github.com>
2020-10-08 21:33:38 -0400

    AndManyMore tag on landing page, cursor on github link (#47)

--- components/LandingPage/PaymentMethodTag.jsx
+++ components/LandingPage/PaymentMethodTag.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const PaymentMethodTag = ({ label, iconName, iconSize, color, tagVariantColor }) => (
     <Box mx={1} my={1}>
         <Tag size="lg" variantColor={tagVariantColor}>
-            <Icon verticalAlign="top" name={iconName} color={color} size={iconSize} mr={2} />
+            {iconName && (<Icon verticalAlign="top" name={iconName} color={color} size={iconSize} mr={2} />)}
             <TagLabel py={1}>{label}</TagLabel>
         </Tag>
     </Box>
