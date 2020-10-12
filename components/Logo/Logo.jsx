bae3f7f18a7c3236c98e8de5f37aac88a7da1459
Mark Jackson <35121685+markjackson02@users.noreply.github.com>
2020-10-11 22:36:44 -0400

    improve and simplify logo responsiveness (#54)

--- components/Logo/Logo.jsx
+++ components/Logo/Logo.jsx
@@ -1,55 +1,40 @@
 import PropTypes from 'prop-types';
 import { Flex, Text as ChakraText, useTheme, Icon } from '@chakra-ui/core';
 
-const Logo = ({ text, logoSize, fontSize, reverse }) => {
+const Logo = ({ text }) => {
     const theme = useTheme();
     const Image = () => (
-        <Icon name="piggyLogo" size={logoSize} />
+        <Icon name="piggyLogo" size={["48px", "64px"]} />
     );
     const Text = () => (
         <ChakraText
-            fontSize={fontSize}
+            fontSize={["4xl", "5xl"]}
             fontFamily="Changa"
             fontWeight={500}
             color={theme.colors.gray['500']}
-            ml={reverse ? 0 : 2}
-            mr={reverse ? 2 : 0}
+            ml={2}
         >
             {text}
         </ChakraText>
     );
     return (
         <Flex
-            ml={2}
+            ml={1}
+            mr={2}
             align="center"
         >
-            {!reverse ? (
-                <>
-                <Image />
-                <Text />
-                </>
-            ) : (
-                <>
-                <Text />
-                <Image />
-                </>
-            )}
+            <Image />
+            <Text />
         </Flex>
     );
 };
 
 Logo.propTypes = {
     text: PropTypes.string,
-    logoSize: PropTypes.arrayOf(PropTypes.string),
-    fontSize: PropTypes.arrayOf(PropTypes.string),
-    reverse: PropTypes.bool,
 };
 
 Logo.defaultProps = {
     text: 'coindrop',
-    logoSize: ["48px", "64px"],
-    fontSize: ["4xl", "5xl"],
-    reverse: false,
 };
 
 export default Logo;
