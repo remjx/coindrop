38cf66f5d35fbebeccbc16e0445cfa27c25f6ad8
Mark Jackson <markjackson02@ymail.com>
2020-11-19 04:17:03 -0500

    use ya in case xa is false

--- utils/auth/mapUserData.js
+++ utils/auth/mapUserData.js
@@ -1,8 +1,8 @@
 export const mapUserData = (user) => {
-  const { uid, email, xa } = user;
+  const { uid, email, xa, ya } = user;
   return {
     id: uid,
     email,
-    token: xa,
+    token: xa || ya, // TODO: This is not the proper way to do it https://github.com/vercel/next.js/pull/18074
   };
 };
