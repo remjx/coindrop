cda848fd710f3d4009467cfe21807bba0c973748
Mark Jackson <markjackson02@ymail.com>
2020-11-28 15:32:17 -0500

    fix cypress env vars

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,9 +7,9 @@ import { attachCustomCommands } from "cypress-firebase";
 export const config = {
   apiKey: Cypress.env("NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY"),
   authDomain: Cypress.env("NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN"),
-  databaseURL: Cypress.env("process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL"),
+  databaseURL: Cypress.env("NEXT_PUBLIC_FIREBASE_DATABASE_URL"),
   projectId: Cypress.env("NEXT_PUBLIC_FIREBASE_PROJECT_ID"),
-  storageBucket: Cypress.env("process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET_URL"),
+  storageBucket: Cypress.env("NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET_URL"),
 };
 
 firebase.initializeApp(config);
