b4b3bde8825d3700eca92312cc28e0292e6e8cdd
Mark Jackson <markjackson02@ymail.com>
2020-12-03 12:43:27 -0500

    update FAQ

--- src/settings.js
+++ src/settings.js
@@ -2,8 +2,10 @@ export const piggybankPathRegex = /^[a-z][\w-]{1,30}[a-z0-9]$/;
 export const maxPiggybanksPerUser = 10;
 
 // Links
+export const coindropEmail = "coindrop.to@gmail.com";
+export const markJacksonWebsite = "https://remjx.com";
 export const githubUrl = 'https://github.com/markjackson02/coindrop';
-export const markJacksonUrl = 'https://twitter.com/markjackson02';
+export const markJacksonUrl = 'https://twitter.com/remjxd';
 export const twitterUrl = "https://twitter.com/coindrop_to";
 export const githubReadmeHelpUrl = "https://github.com/markjackson02/coindrop#contributions-%EF%B8%8F%EF%B8%8F";
 export const githubPrivacyPolicyUrl = "https://github.com/markjackson02/coindrop/blob/master/PRIVACY_POLICY.md";
