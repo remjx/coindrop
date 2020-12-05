e4ed5108cd691183f7ca7bb4ade08e88136c4da6
Mark Jackson <markjackson02@ymail.com>
2020-12-04 20:12:32 -0500

    remove unnecessary cookie.get()s

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -20,13 +20,13 @@ const Dashboard: FunctionComponent = () => {
     const [isCreateTriggered, setIsCreateTriggered] = useState(false);
     const [candidatePiggybankPath, setCandidatePiggybankPath] = useState();
     const { submitStatus } = useCreatePiggybank(candidatePiggybankPath, setCandidatePiggybankPath, user, isCreateTriggered, setIsCreateTriggered);
-    const pendingLoginCreatePiggybankPath = cookies.get('pendingLoginCreatePiggybankPath');
     useDidMountEffect(() => {
         if (!user) {
             router.push('/');
         }
     }, [user]);
     useEffect(() => {
+        const pendingLoginCreatePiggybankPath = cookies.get('pendingLoginCreatePiggybankPath');
         if (pendingLoginCreatePiggybankPath) {
             setCandidatePiggybankPath(pendingLoginCreatePiggybankPath);
             setIsCreateTriggered(true);
