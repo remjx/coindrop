462a2e74faaaae49930eb044a9be6da6bfc1233b
Mark Jackson <markjackson02@ymail.com>
2020-12-09 16:13:04 -0500

    update ComponentMapping. remove launch post draft. set TL;DR to h2

--- components/Blog/ComponentMapping.tsx
+++ components/Blog/ComponentMapping.tsx
@@ -1,11 +1,10 @@
 import Image from 'next/image';
-import { Center, Heading, Text, Button, Link, UnorderedList, ListItem, OrderedList } from '@chakra-ui/react';
+import { Center, Heading, Text, Link, UnorderedList, ListItem, OrderedList } from '@chakra-ui/react';
 
 export const components = {
     h2: ({ children }) => <Heading as="h2" my="1.5rem" size="xl">{children}</Heading>,
     h3: ({ children }) => <Heading as="h3" my="1.5rem" size="lg">{children}</Heading>,
-    h4: ({ children }) => <Heading as="h4">{children}</Heading>,
-    button: Button,
+    h4: ({ children }) => <Heading as="h4" my="1.5rem" size="md">{children}</Heading>,
     p: ({ children }) => <Text mb="1.5rem" fontSize="lg">{children}</Text>,
     Center,
     a: Link,
