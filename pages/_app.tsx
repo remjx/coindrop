bac81927ed351fde4f24a95eef71461be973afe7
Mark Jackson <markjackson02@ymail.com>
2020-12-20 00:37:30 -0500

    comment out manual page_view events, it seems to be working automatically with firebase.analytics()

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,30 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { ChakraProvider } from '@chakra-ui/react';
 import { DefaultSeo } from 'next-seo';
-import { FunctionComponent, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import {
+  FunctionComponent,
+  // useEffect
+} from 'react';
+// import { useRouter } from 'next/router';
 import { AppProps } from 'next/app';
 import theme from '../components/theme';
 import SEO from '../src/next-seo.config';
 
 const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag('event', 'page_view', {
-        page_path: url,
-      });
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
+  /* This may not be needed, it looks like Google Analytics is double-counting when this is enabled */
+  // useEffect(() => {
+  //   const handleRouteChange = (url: URL) => {
+  //     gtag('event', 'page_view', {
+  //       page_path: url,
+  //     });
+  //   };
+  //   router.events.on("routeChangeComplete", handleRouteChange);
+  //   return () => {
+  //     router.events.off("routeChangeComplete", handleRouteChange);
+  //   };
+  // }, [router.events]);
   return (
     <ChakraProvider theme={theme}>
         <DefaultSeo {...SEO} />
