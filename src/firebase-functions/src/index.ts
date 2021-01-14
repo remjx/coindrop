306d166df01e52ad550a7ca2c58f176e76b4cd49
Mark Jackson <markjackson02@ymail.com>
2021-01-14 09:25:05 -0500

    getting path error

---
+++ src/firebase-functions/src/index.ts
@@ -0,0 +1,13 @@
+import * as functions from "firebase-functions";
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+export const initializeUser = functions.auth.user().onCreate((user) => {
+    // 
+});
