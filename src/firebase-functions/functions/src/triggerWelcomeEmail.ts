7605aad9aa21c809cc4929ed49db32eb4a88fda5
Mark Jackson <markjackson02@ymail.com>
2021-01-15 03:39:22 -0500

    add logs

--- src/firebase-functions/functions/src/triggerWelcomeEmail.ts
+++ src/firebase-functions/functions/src/triggerWelcomeEmail.ts
@@ -20,11 +20,27 @@ export const triggerWelcomeEmail = functions.auth.user().onCreate(async (user) =
         },
         {
           headers: {
-            FIREBASE_FUNCTIONS_AUTH_TOKEN_ENCRYPTED,
+            FIREBASE_FUNCTIONS_AUTH_TOKEN_ENCRYPTED
           },
         },
     );
   } catch (error) {
     functions.logger.error(error);
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      functions.logger.error(error.response.data);
+      functions.logger.error(error.response.status);
+      functions.logger.error(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      functions.logger.error(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      functions.logger.error("Error", error.message);
+    }
+    functions.logger.error(error.config);
   }
 });
