fc0de71df330a6ed81cfb41b6c4e3a6ea07d9152
Mark Jackson <markjackson02@ymail.com>
2021-01-15 01:43:21 -0500

    fix lint

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,21 +1,11 @@
 import { FunctionComponent } from 'react';
 import { useRouter } from 'next/router';
 import { NextSeo } from 'next-seo';
-import axios from 'axios';
-import dayjs from 'dayjs';
 import { Text } from '@chakra-ui/react';
-import firebase from 'firebase/app';
 import { useUser } from '../../utils/auth/useUser';
 import useDidMountEffect from '../../utils/hooks/useDidMountEffect';
 import UserOwnedPiggybanks from './UserOwnedPiggybanks/UserOwnedPiggybanks';
 import { withDefaultLayout } from '../Layout/DefaultLayoutHOC';
-import { initializeUserData } from '../../src/db/mutations/user/initialize-user-data';
-import { User } from '../../utils/auth/mapUserData';
-
-const sendWelcomeEmail = (user: User) => {
-    const { token } = user;
-    axios.get('/api/email/welcome', { headers: { token }});
-};
 
 const Dashboard: FunctionComponent = () => {
     const router = useRouter();
