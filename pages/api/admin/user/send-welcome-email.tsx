1e87cba06725139c4dc0a73f48c9d2b8b6a5ecaa
Mark Jackson <markjackson02@ymail.com>
2021-01-17 18:08:58 -0500

    update email wording

--- pages/api/admin/user/send-welcome-email.tsx
+++ pages/api/admin/user/send-welcome-email.tsx
@@ -17,13 +17,13 @@ const sendWelcomeEmail: NextApiHandler = async (req: NextApiRequest, res: NextAp
               Coindrop&apos;s mission is to make it easy to receive tips and donations anywhere.
           </Paragraph>
           <Paragraph>
-              As you begin to use Coindrop, I&apos;d love to hear your feedback. What could be improved? You can reply directly to any of our e-mail.
+              As you begin to use Coindrop, I&apos;d love to hear your feedback. What could be improved? You can reply directly to any of our e-mails.
           </Paragraph>
           <Paragraph>
-              Coindrop is 100% open-source on <a href={githubUrl} style={{color: "gray"}}>Github</a> so any contributions are open and welcome.
+              Coindrop is 100% open-source on <a href={githubUrl} style={{color: "gray"}}>Github</a> so any contributions and ideas are open and welcome.
           </Paragraph>
           <Paragraph>
-              Thank you and enjoy zero-fee tipping!
+              Thanks and enjoy zero-fee payments!
           </Paragraph>
           <Paragraph>
               - Mark Jackson, Founder of Coindrop
