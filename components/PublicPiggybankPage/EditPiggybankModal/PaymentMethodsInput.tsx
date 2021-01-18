bb10b76d6f472c45dcb0ea1ac7ad1b7c4ee7a5ad
Mark Jackson <35121685+remjx@users.noreply.github.com>
2021-01-18 03:43:09 -0500

    only show payment method not listed on new payment method. remove verbosity from Remove button on existing payment methods (#229)

--- components/PublicPiggybankPage/EditPiggybankModal/PaymentMethodsInput.tsx
+++ components/PublicPiggybankPage/EditPiggybankModal/PaymentMethodsInput.tsx
@@ -60,29 +60,29 @@ const PaymentMethodsInput: FC<Props> = ({ fieldArrayName, fields, control, regis
     const containsInvalidAddress = paymentMethodsDataWatch.some(paymentMethod => !paymentMethod.address);
     const { isAddressTouched, setIsAddressTouched } = useContext(AdditionalValidation);
     // optgroup not compatible with Chakra dark mode: https://github.com/chakra-ui/chakra-ui/issues/2853
-    const optionsGroup = (category: Category) => {
-        const optgroupLabels: Record<Category, string> = {
-            "digital-wallet": 'Digital Wallets',
-            "digital-asset": "Digital Assets",
-            "subscription-platform": "Subscription Platforms",
-        };
-        return (
-            <optgroup label={optgroupLabels[category]}>
-                {paymentMethods
-                .filter(paymentMethod => paymentMethod.category === category)
-                .sort((a, b) => (a.id < b.id ? -1 : 1))
-                .map(({ id, displayName }) => (
-                    <option
-                        key={id}
-                        value={id}
-                        style={{display: paymentMethodsDataWatch.map(paymentMethodDataWatch => paymentMethodDataWatch.paymentMethodId).includes(id) ? "none" : undefined }}
-                    >
-                        {displayName}
-                    </option>
-                ))}
-            </optgroup>
-        );
-    };
+        // const optionsGroup = (category: Category) => {
+        //     const optgroupLabels: Record<Category, string> = {
+        //         "digital-wallet": 'Digital Wallets',
+        //         "digital-asset": "Digital Assets",
+        //         "subscription-platform": "Subscription Platforms",
+        //     };
+        //     return (
+        //         <optgroup label={optgroupLabels[category]}>
+        //             {paymentMethods
+        //             .filter(paymentMethod => paymentMethod.category === category)
+        //             .sort((a, b) => (a.id < b.id ? -1 : 1))
+        //             .map(({ id, displayName }) => (
+        //                 <option
+        //                     key={id}
+        //                     value={id}
+        //                     style={{display: paymentMethodsDataWatch.map(paymentMethodDataWatch => paymentMethodDataWatch.paymentMethodId).includes(id) ? "none" : undefined }}
+        //                 >
+        //                     {displayName}
+        //                 </option>
+        //             ))}
+        //         </optgroup>
+        //     );
+        // };
     return (
         <>
         {fields.length < 1
@@ -116,7 +116,7 @@ const PaymentMethodsInput: FC<Props> = ({ fieldArrayName, fields, control, regis
                                 <Flex flex="1" textAlign="left" align="center">
                                     <Flex mr={1} align="center">
                                         {PaymentMethodIcon ? <PaymentMethodIcon mr={2} /> : <QuestionOutlineIcon mr={2} />}
-                                        {paymentMethodNames[watchedData?.paymentMethodId] ?? 'New payment method'}
+                                        {paymentMethodNames[watchedData?.paymentMethodId] ?? 'Add payment method'}
                                     </Flex>
                                     {watchedData?.isPreferred && (
                                         <Flex>
@@ -201,16 +201,18 @@ const PaymentMethodsInput: FC<Props> = ({ fieldArrayName, fields, control, regis
                                     </FormControl>
                                 </Box>
                                 <Flex
-                                    justify="space-between"
+                                    justify={watchedData?.paymentMethodId === 'default-blank' ? 'space-between' : 'flex-end'}
                                     mt={3}
                                     wrap="wrap"
                                     align="center"
                                 >
-                                    <Text fontSize="xs" ml={1}>
-                                        <Link href="/blog/payment-method-request" isExternal>
-                                            Payment method not listed?
-                                        </Link>
-                                    </Text>
+                                    {watchedData?.paymentMethodId === 'default-blank' && (
+                                        <Text fontSize="xs" ml={1}>
+                                            <Link href="/blog/payment-method-request" isExternal>
+                                                Payment method not listed?
+                                            </Link>
+                                        </Text>
+                                    )}
                                     <Button
                                         onClick={() => {
                                             remove(index);
@@ -220,7 +222,7 @@ const PaymentMethodsInput: FC<Props> = ({ fieldArrayName, fields, control, regis
                                         size="sm"
                                     >
                                         {'Remove '}
-                                        {paymentMethodNames[watchedData?.paymentMethodId]}
+                                        {/* {paymentMethodNames[watchedData?.paymentMethodId]} */}
                                     </Button>
                                 </Flex>
                             </AccordionPanel>
