7c0a70e7ea6ae396e50ce27b172e90711b5ae055
Mark Jackson <markjackson02@ymail.com>
2021-01-20 11:31:21 -0500

    remove cy.pause

--- cypress/integration/app/configure-coindrop.ts
+++ cypress/integration/app/configure-coindrop.ts
@@ -32,7 +32,6 @@ describe('Configure Coindrop', () => {
             .type(name);
         cy.get('button#add-payment-method-button')
             .click();
-        cy.pause();
         cy.get('[data-cy=select-payment-method-container-default-blank]')
             .find('select')
             .select(paymentMethodValue);
